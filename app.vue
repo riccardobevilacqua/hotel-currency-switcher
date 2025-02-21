def0b385aec65f501416a56cfc80d65265d8900d
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2025-02-21 14:49:35 +0100

    Updated default locale handling

--- app.vue
+++ app.vue
@@ -20,7 +20,7 @@
     <div class="hotelGrid">
       <div v-for="hotel in hotels" :key="hotel.id" class="hotelCard">
         <div class="hotelImage">
-          <span class="availabilityBadge">available</span>
+          <span class="availabilityBadge" v-if="hotel.availableHotels > 0">available</span>
           <button class="expandButton">
             <svg width="24" height="24" viewBox="0 0 24 24">
               <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
@@ -46,10 +46,17 @@
 
 <script setup>
 const DEFAULT_CURRENCY_CODE = 'EUR'
-const DEFAULT_LOCALE = 'en-NL'
+const DEFAULT_LOCALE = 'en-US'
+
+const locale = ref(DEFAULT_LOCALE)
 const selectedCurrency = ref(DEFAULT_CURRENCY_CODE)
+
 const { data: hotels } = await useFetch('/api/offers')
 const { data: exchangeRates } = await useFetch('/api/exchange-rates')
+
+onMounted(() => {
+  locale.value = navigator.language || DEFAULT_LOCALE
+})
 
 const availableCurrencies = computed(() => {
   if (!exchangeRates.value) return []
@@ -67,7 +74,7 @@ const currencySymbolToCode = {
 }
 
 const formatPrice = (currencyCode, amount) => {
-  return new Intl.NumberFormat(DEFAULT_LOCALE, {
+  return new Intl.NumberFormat(locale.value, {
     style: 'currency',
     currency: currencyCode,
     currencyDisplay: 'narrowSymbol'
