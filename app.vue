24ab8d3e4e222bd62c5ea930e0a735807e318ca4
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2025-02-21 15:19:06 +0100

    Added nuxt image

--- app.vue
+++ app.vue
@@ -26,7 +26,7 @@
               <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
             </svg>
           </button>
-          <img :src="hotel.image" :alt="`${hotel.city} view`">
+          <NuxtImg :src="hotel.image" :alt="`${hotel.city} view`" />
         </div>
         <div class="hotelInfo">
           <h2>{{ hotel.city }}</h2>
