f93c261d1086f0a8ac3b787158dd568f084cfb58
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2025-02-21 19:36:10 +0100

    Updated configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   modules: ['@nuxt/image'],
   image: {
+    provider: 'unsplash',
     domains: ['images.unsplash.com']
   }
 })
